82860caa09b136ab27b363ade8dc10ec591c529a
masterwhipper <master_whipper@aol.com>
2014-05-10 23:11:18 -0500

    hai

--- public_html/js/time.js
+++ public_html/js/time.js
@@ -1,18 +1,18 @@
 var timeC = 0;
 
-function timeSet(){
-	switch (timeC) {
-   		case 40:
-    		timeC = 0;
-    		$('#content').append(' ' + timeC);
-    		break;
-  		default:
-    		timeC++;
-    		$('#content').append(' ' + timeC);
-    		break;
-   }
+function timeSet() {
+    switch (timeC) {
+        case 40:
+            timeC = 0;
+            $('#content').append(' ' + timeC);
+            break;
+        default:
+            timeC++;
+            $('#content').append(' ' + timeC);
+            break;
+    }
 }
 
-function timeRun(){
- setInterval(timeSet, 10);
+function timeRun() {
+    setInterval(timeSet, 10);
 }
